3a053c0c850f07d703e1af50e74db5a0c2564593
cavy <cavy1010@163.com>
2023-01-26 14:35:09 +0800

    增加框架程序

---
+++ src/HAMS.Frame.Kernel/Controls/PasswordTextBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Controls;
+
+namespace HAMS.Frame.Kernel.Controls
+{
+    public class PasswordTextBox : TextBox
+    {
+        public string PasswordText
+        {
+            get { return (string)GetValue(PasswordTextProperty); }
+            set { SetValue(PasswordTextProperty, value); }
+        }
+
+        public static DependencyProperty PasswordTextProperty =
+            DependencyProperty.Register("PasswordText", typeof(string), typeof(PasswordTextBox), new PropertyMetadata(string.Empty));
+
+        public PasswordTextBox()
+        {
+            Binding binding = new Binding();
+            binding.Converter = new PasswordTextConverter();
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Path = new PropertyPath("PasswordText");
+            binding.Source = this;
+            BindingOperations.SetBinding(this, TextProperty, binding);
+        }
+    }
+}
